4cf60cf9003aae8557e3f98e63c51a07923f1404
Matt Pivnick <mpivnick@ebsco.com>
2018-04-26 06:55:48 -0400

    removing this reference

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 
-import Header from '../components/header';
 import Navigation from '../components/navigation';
 import "font-awesome/scss/font-awesome.scss";
 import './index.scss';
