ec82bcd2a6e2a947450634887d472347f9b61859
Matt Pivnick <mpivnick@ebsco.com>
2018-04-25 20:05:04 -0400

    adding some plugins for PWA-age

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,5 +14,18 @@ module.exports = {
     },
     'gatsby-transformer-remark',
     'gatsby-transformer-json',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Pokemon Go Reference Guide",
+        short_name: "PogoRefGuide",
+        start_url: "/",
+        background_color: "#21C0FB",
+        theme_color: "#21C0FB",
+        display: "standalone",
+        icon: "/images/favicon.ico", // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-offline'
   ],
 }
